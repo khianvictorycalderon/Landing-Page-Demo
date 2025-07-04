e22960f5c1b9e25350059fbb7631277063cb51be
khian calderon <ckhianvictory@gmail.com>
2025-07-04 16:08:49 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+function App() {
+  
+  useEffect(() => {
+    // alert("This is only a demo for a landing page and is not a real business.");
+  },[]);
+  
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
